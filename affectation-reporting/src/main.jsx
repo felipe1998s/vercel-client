076d813c5a4c9e053a3da78a303d1c7a6b8ddd15
felipe1998s <felipeandressuarez20@gmail.com>
2023-03-15 20:25:04 -0500

    second commit

--- affectation-reporting/src/main.jsx
+++ affectation-reporting/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
 import store from './redux/Store/store';
+import axios from 'axios';
+axios.defaults.baseURL="http://localhost:3000";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
