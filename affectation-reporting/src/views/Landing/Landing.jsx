003307c822ad44135bea27ba3ef51cee9fa70812
felipe1998s <felipeandressuarez20@gmail.com>
2023-06-27 14:09:57 -0500

    fix: made changes to landing view

--- affectation-reporting/src/views/Landing/Landing.jsx
+++ affectation-reporting/src/views/Landing/Landing.jsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom";
 import style from "./Landing.module.css";
 import { leer } from "../../funtions/leer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux"
+import { getAffecteds } from "../../redux/Actions/actions";
 export const Landing = () => {
     const text = leer();
     const [string,setString]=useState(text);
+
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getAffecteds())
+    },[dispatch]);
+
     
     
 
